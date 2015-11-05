2ced71fcff3ac2dfbe8bc50e399a55f125e0fb09
Raspen0 <Raspen0@users.noreply.github.com>
2015-11-05 10:44:02 +0100

    Switch to Spigot Repo and change package names

--- src/main/java/nl/raspen0/RaspenEssentials/CommandGamemode.java
+++ src/main/java/nl/raspen0/RaspenEssentials/CommandGamemode.java
@@ -1,4 +1,4 @@
-package com.gmail.rickvinke1.raspenessentials;
+package nl.raspen0.RaspenEssentials;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
--- src/main/java/nl/raspen0/RaspenEssentials/CommandSpawn.java
+++ src/main/java/nl/raspen0/RaspenEssentials/CommandSpawn.java
@@ -1,4 +1,4 @@
-package com.gmail.rickvinke1.raspenessentials;
+package nl.raspen0.RaspenEssentials;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
